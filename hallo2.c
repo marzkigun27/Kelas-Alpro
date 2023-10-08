bd0b85d4305ec6413c1f4e446949952e86d8b345
marzkigun27 <umarkigun@gmailcom>
2023-10-08 23:11:09 +0700

    edited

--- hallo2.c
+++ hallo2.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hallo baaajjhjjnnnjjnjjnggg");
-  return 0;
+	int m,n,r;
+	printf("Masukan nilai M : ");
+	scanf("%d",&m);
+	printf("Masukan nilai N : ");
+	scanf("%d",&n);
+
+	while (n != 0) {
+		//printf("Proses ke : %d\n",i);
+		r = m%n;
+		//printf("hasil dari M modulus N : %d\n",r);
+		m=n;
+		//printf("nilai M yang baru : %d\n",m);
+		n=r;
+		//printf("nilai N yang baru : %d\n",n); 
+		//i+=1;
+	}
+	printf("Nilai GCD nya adalah : %d\n",m);
 }
