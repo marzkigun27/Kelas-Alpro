e55fefb9342d115526f17cb8612f171953b562e9
marzkigun27 <umarkigun@gmail.com>
2023-10-26 23:14:51 +0700

    masukan file GCD

---
+++ Tugas02-GCD/GCD.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	int m,n,r;
+	printf("Masukan nilai M : ");
+	scanf("%d",&m);
+	printf("Masukan nilai N : ");
+	scanf("%d",&n);
+
+	while (n != 0) {
+		//printf("Proses ke : %d\n",i);
+		r = m%n;
+		//printf("hasil dari M modulus N : %d\n",r);
+		m=n;
+		//printf("nilai M yang baru : %d\n",m);
+		n=r;
+		//printf("nilai N yang baru : %d\n",n); 
+	}
+	if (m<0){
+		m*=-1;
+	}
+	printf("Nilai GCD nya adalah : %d\n",m);
+}
