ba5551e041015eb528a84b812e0ed2d7809d0806
marzkigun27 <umarkigun@gmail.com>
2023-10-31 15:22:50 +0700

    edit input menu

--- Tugas06/Integral.c
+++ Tugas06/Integral.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <stdlib.h>
 double fungsi_utama(double a) {
-    return 6*a*a*a+2;
+    return 6*pow(a,3)+2;
 }
 
 void main(){
     int jumlah_subinterval;
     double hasil=0.0,batas_bawah,batas_atas,delta_x,x_i;
 
-    printf("Masukan batas atas : "); scanf("%lf",&batas_atas);
-    printf("Masukan batas bawah : "); scanf("%lf",&batas_bawah);
-    printf("Masukan Jumlah subinterval: "); scanf("%d",&jumlah_subinterval);
+    do
+    {
+        printf("Masukan batas atas : "); scanf("%lf",&batas_atas);
+        printf("Masukan batas bawah : "); scanf("%lf",&batas_bawah);
+        printf("Masukan Jumlah subinterval: "); scanf("%d",&jumlah_subinterval);
+        system("clear");
+        printf("Jumlah interval tidak boleh negatif!!!\n\n");
+        printf("Masukan angka lain!!\n\n");
+    } while (jumlah_subinterval < 0);
+    system("clear");
 
     delta_x = (batas_atas-batas_bawah)/jumlah_subinterval;
-
+    
     int i = 1;
 
     while (i<=jumlah_subinterval)
